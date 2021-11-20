dba2ff0d6cebb6732a0ea78157e40057191d1111
Igor Golm <igor.golm@devecto.com>
2021-11-20 17:41:41 +0200

    Added ubuntu-kube-proxy instance module

---
+++ instance/eu-central-1/ubuntu-kube-proxy/variables.tf
@@ -0,0 +1,79 @@
+variable "profile" {
+  default = "default"
+}
+
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+}
+
+variable "key_name" {
+  default = "admin_key_frankfurt"
+}
+
+variable "ami" {
+  default = "ami-0a49b025fffbbdac6"
+}
+
+variable "monitoring" {
+  default = "false"
+}
+
+variable "ebs_optimized" {
+  default = "false"
+}
+
+variable "public_ip" {
+  default = "true"
+}
+
+variable "disk_termination" {
+  default = "false"
+}
+
+variable "volume_type" {
+  default = "gp2"
+}
+
+variable "availability_zone" {
+  default = "eu-central-1a"
+}
+
+variable "api_termination" {
+  default = "false"
+}
+
+variable "name" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "subnet_id" {
+  default = ""
+}
+
+variable "root_vsize" {
+  default = ""
+}
+
+variable "number" {
+  default = 1
+}
+
+variable "interface_enabled" {
+  default = 0 
+}
+
+variable "net_device_index" {
+  default = 1
+}
+
+variable "eip_enabled" {
+  default = 0
+}
